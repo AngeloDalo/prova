a12b8f7811d609867898a17dc5b100364d37b110
AngeloDalo <angelo251158@gmail.com>
2022-05-06 16:50:52 +0200

    griglia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { LogoutComponent } from './pages/logout/logout.component';
 import { NgModule } from '@angular/core';
 import { RouteGuardService } from '../services/route-guard.service';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { GridArticoliComponent } from './pages/grid-articoli/grid-articoli.component';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'login', component: LoginComponent},
   {path:'welcome/:userid', component: WelcomeComponent, canActivate:[RouteGuardService]},
   {path:'articoli', component : ArticoliComponent, canActivate:[RouteGuardService]},
+  {path:'articoli/grid', component : GridArticoliComponent, canActivate:[RouteGuardService]},
   {path:'logout', component : LogoutComponent},
   {path:'**', component: ErrorComponent},
 ];
